2509c090874b0b322f9273e79671bf81093f92f3
Lata Sarsu <lata.sarsu@gyansys.com>
2024-11-04 12:27:25 +0000

    First commit

---
+++ server.mjs
@@ -0,0 +1,24 @@
+import net from 'net';
+let sockets = [];
+
+function broadcast(data) {
+    sockets.forEach(socket => {
+        socket.write(data);
+    })
+}
+
+const server = net.createServer(function (socket) {
+    sockets.push(socket);
+
+    socket.on('data', function (data) {
+        broadcast(data);
+    });
+
+    socket.on('error', function (err) {
+        console.log(err);
+    });
+})
+
+server.listen(8080, function(){
+    console.log("Server is listening");
+});
